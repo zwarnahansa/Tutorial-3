cfa4f5c8a7cf42316987afb8f461b5ab16885082
zwarnahansa <it18211474@my.sliit.lk>
2020-03-26 22:55:47 +0530

    Tutorial03

---
+++ app/src/main/java/com/example/tute03/Fragment2.java
@@ -0,0 +1,18 @@
+package com.example.tute03;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_fragment2, container, false);
+    }
+}
